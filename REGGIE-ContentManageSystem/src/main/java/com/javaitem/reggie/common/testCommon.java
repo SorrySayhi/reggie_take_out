da878f3f020fd61bb591854b5ad116d72af3d894
antic <antic@cc.com>
2022-12-06 06:07:37 +0800

    11

---
+++ REGGIE-ContentManageSystem/src/main/java/com/javaitem/reggie/common/testCommon.java
@@ -0,0 +1,13 @@
+package com.javaitem.reggie.common;
+
+/**
+ * @Author win
+ * @create 2022/12/6 5:34
+ */
+public class testCommon {
+
+
+    public static void main(String[] args) {
+        System.out.println("hehlo");
+    }
+}
